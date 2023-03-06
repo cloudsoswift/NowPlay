fd501fe8993e52747ad5737a153389f1ede69a40
sangjun bae <qotkdwns1324@naver.com>
2023-03-06 16:19:24 +0900

    feat: Frontend 폴더구조

--- Front/pjt_front/src/pages/NaverCallBackPage.tsx
+++
@@ -1,7 +0,0 @@
-const NaverCallBackPage = () => {
-    return <>
-    <p>로그인 완료된 페이지입니다!!!!!!!!!!!!!!!!!</p>
-    </>
-}
-
-export default NaverCallBackPage
--- Front/pjt_front/src/pages/WelcomePage.tsx
+++
@@ -1,8 +0,0 @@
-const Welcome = () => {
-    return (<>
-        <h1>Welcome입니다!</h1>
-        <button>Naver Login</button>
-    </>)
-}
-
-export default Welcome

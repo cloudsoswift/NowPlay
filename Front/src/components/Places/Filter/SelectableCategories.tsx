aad35edb144a5aedeb3ef26b794af46a9c019d54
cloudsoswift <magic9282m@gmail.com>
2023-03-17 18:01:20 +0900

    feat: 카테고리 임시 표시

--- Front/src/components/Places/Filter/SelectableCategories.tsx
+++ Front/src/components/Places/Filter/SelectableCategories.tsx
@@ -1,29 +1,64 @@
+import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { filterState } from "../Map";
-import type { TMainCategory } from "../Map";
+import { TMainCategory, TSubCategory } from "../Types";
 type sCategoryProps = {
-  category: TMainCategory,
+  category: TMainCategory;
+  onClick: Function;
+  id: number;
+};
+type subCategoryList = {
+  id: number;
+  subCategories: Array<TSubCategory>;
 };
 export const SelectableCategory = (props: sCategoryProps) => {
   const handleSelectCategory = () => {
-
-  }
+    props.onClick({
+      id: props.id,
+      subCategories: props.category.subCategory,
+    });
+  };
   return (
-    <div>
-
+    <div onClick={handleSelectCategory}>
+      <img src={`/svg/${props.category.imageURL}`} />
+      {props.category.category}
     </div>
   );
 };
 
-type sCategoriesProps = {
-  
-};
+type sCategoriesProps = {};
 export const SelectableCategories = (props: sCategoriesProps) => {
   const { categories } = useRecoilValue(filterState);
+  const [subCategories, setSubcategories] = useState<subCategoryList>();
+  useEffect(() => {
+    console.log(subCategories);
+  }, [subCategories]);
   return (
-    <div className="bg-white border-4">
-      {categories.map((category)=>{
-        return <SelectableCategories category={category}/>
+    <div className="bg-white border-4 grid grid-cols-3">
+      {categories.map((category, index) => {
+        return (index + 1) % 3 !== 0 ? (
+          <SelectableCategory
+            id={index}
+            category={category}
+            onClick={setSubcategories}
+          />
+        ) : (
+          <>
+            <SelectableCategory
+              id={index}
+              category={category}
+              onClick={setSubcategories}
+            />
+            {subCategories?.id != undefined &&
+              Math.floor(subCategories.id / 3) === Math.floor(index / 3) && (
+                <div className="col-span-3">
+                  {subCategories?.subCategories.map((sbC) => {
+                    return <div>{sbC.category}</div>;
+                  })}
+                </div>
+              )}
+          </>
+        );
       })}
     </div>
   );

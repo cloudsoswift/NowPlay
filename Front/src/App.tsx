90b0bf4cd182aba1d2415683d77476f78ef39585
sangjun bae <qotkdwns1324@naver.com>
2023-03-20 17:03:23 +0900

    Fix: 유저, 사장페이지 분리

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -4,54 +4,24 @@ import {
   Routes,
   useLocation,
 } from "react-router-dom";
-import PrivateRoute from "./utils/PrivateRouter";
-import MobileNavbar from "./components/Navbar/MobileNavbar";
-import LoginPage from "./pages/user/LoginPage";
-import SignupPage from "./pages/user/SignupPage";
-import MyPage from "./pages/user/MyPage";
 
 import GlobalStyle from "./assets/GlobalStyle";
 import styled from "styled-components";
-import DeskNavbar from "./components/Navbar/DeskNavBar";
+
+import Mobile from "./Mobile";
+import Desktop from "./Desktop";
 
 function App() {
-  const location = useLocation();
-
-  if (location.pathname.includes("owner")) {
-    return (
-      <>
-        오너페이지입니당
-        <GlobalStyle />
-        <Routes>
-        </Routes>
-        <DeskNavbar />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <GlobalStyle />
-        <MobileContainer>
-          <Routes>
-            <Route element={<PrivateRoute authentication={false} />}>
-              <Route path="/mypage/login" element={<LoginPage />} />
-            </Route>
-            <Route element={<PrivateRoute authentication={false} />}>
-              <Route path="/mypage/signup" element={<SignupPage />} />
-            </Route>
-            <Route element={<PrivateRoute authentication={true} />}>
-              <Route path="/mypage/*" element={<MyPage />} />
-            </Route>
-          </Routes>
-        </MobileContainer>
-        <MobileNavbar />
-      </>
-    );
-  }
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/owner/*" element={<Desktop />} />
+        <Route path="/*" element={<Mobile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
 
-const MobileContainer = styled.div`
-  padding-bottom: 80px;
-`;

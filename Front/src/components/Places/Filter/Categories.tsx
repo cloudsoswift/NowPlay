2f4b8e5677ad3d2be91d4416846616c26f4da477
cloudsoswift <magic9282m@gmail.com>
2023-03-20 13:36:59 +0900

    style: '선택된 소분류 카테고리'에 대한 목록 스켈레톤 스타일 지정

--- Front/src/components/Places/Filter/Categories.tsx
+++ Front/src/components/Places/Filter/Categories.tsx
@@ -2,26 +2,27 @@
 import * as React from 'react';
 import { useRecoilValue } from 'recoil';
 import { filterState } from '../Map';
+import { TSubCategory } from '../Types';
 type CategoriesProps = {
 
 };
 type CategoryProps = {
-  
+  data: TSubCategory
 };
 
 export const Category = (props: CategoryProps) => {
   return (
-    <div className="rounded-full">
-      카테고리
-    </div>
+    <span className="rounded-full mx-2">
+      {props.data.category}
+    </span>
   );
 };
 
 export const Categories = (props: CategoriesProps) => {
   const { selectedCategories } = useRecoilValue(filterState);
   return (
-    <div className="inline">
-      {selectedCategories.map((data)=><Category key={data.category}/>)}
+    <div className="w-full overflow-x-scroll">
+      {selectedCategories.map((data)=><Category key={data.category} data={data}/>)}
     </div>
   );
 };

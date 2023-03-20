f1391925255f204aec1f700cd9cea14172466d4d
cloudsoswift <magic9282m@gmail.com>
2023-03-20 18:01:07 +0900

    feat: 사업장 상세 정보 페이지 일단 생성

--- Front/src/pages/places/PlaceDetailPage.tsx
+++ Front/src/pages/places/PlaceDetailPage.tsx
@@ -1,12 +1,45 @@
-// @flow 
-import * as React from 'react';
+import {useState} from 'react';
+import { PlaceDetailInfo } from '../../components/Places/Detail/PlaceDetailInfo';
+import { PlaceMapInfo } from '../../components/Places/Detail/PlaceMapInfo';
+import { ReviewInfo } from '../../components/Places/Detail/ReviewInfo';
+import { TPlaceDetail } from '../../components/Places/Types';
 type Props = {
   
 };
 export const PlaceDetailPage = (props: Props) => {
+  const TEST_DATA:TPlaceDetail = {
+    imageURL: "place_test_image.png",
+    name: "스파크 노래타운",
+    subCategory: "노래방",
+    address: "경북 구미시 인동중앙로1길 5",
+    distance: 0.513,
+    averageRating: 4.0,
+    reviewCount: 4,
+    isBookmark: true,
+    businessTimes: [],
+    contactNumber: "010-1234-5678",
+    homepage: "http://www.ssafy.com/",
+    imageURLs: ["", "", ""],
+    latitude: 36.107106,
+    longitude: 128.416199,
+    description: "설명이에요",
+    isClosedOnHolidays: false,
+  }
+  const [tabIndex, setTabIndex] = useState(0);
+  const tabButton = "border rounded-xl p-2"
+  const selectedTabButton = `${tabButton} bg-[var(--primary-color)] text-white`
   return (
     <div>
-      
+      <div className="space-x-2">
+        <button className={tabIndex === 0 ? selectedTabButton : tabButton} onClick={()=>{setTabIndex(0)}}>가게 정보</button>
+        <button className={tabIndex === 1 ? selectedTabButton : tabButton} onClick={()=>{setTabIndex(1)}}>위치 / 주차</button>
+        <button className={tabIndex === 2 ? selectedTabButton : tabButton} onClick={()=>{setTabIndex(2)}}>리뷰</button>
+      </div>
+      <div>
+        {tabIndex === 0 && <PlaceDetailInfo placeDetail={TEST_DATA}/>}
+        {tabIndex === 1 && <PlaceMapInfo />}
+        {tabIndex === 2 && <ReviewInfo />}
+      </div>
     </div>
   );
 };

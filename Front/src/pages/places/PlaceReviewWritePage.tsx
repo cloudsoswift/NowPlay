d9ffc315d227e27db5520bef6d85321c3635caff
박규영 <magic9282m@gmail.com>
2023-03-22 01:44:02 +0900

    feat: 리뷰 작성 위한 별점, 내용, 첨부 이미지 state 세팅 및 연결

--- Front/src/pages/places/PlaceReviewWritePage.tsx
+++ Front/src/pages/places/PlaceReviewWritePage.tsx
@@ -1,24 +1,67 @@
-import {useState} from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 type Props = {};
 export const PlaceReviewWritePage = (props: Props) => {
-  const reviewContent = useState("");
-  const handleSubmit = (e:SubmitEvent) => {
-
-  }
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState("");
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== null) {
+      setImageFile(e.target.files[0]);
+    }
+  };
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
   return (
-    <div className="mx-5 grid justify-center">
-      <div>
-        별점
-      </div>     
-      <div className="w-[calc(100%-2.5rem)]">
-        <textarea className="w-full" />
+    <form action="post" className="px-5 grid justify-center space-y-5" onSubmit={handleSubmit}>
+      <div className="[&>*>*]:text-2xl">
+        <button
+          onClick={() => {
+            setRating(1);
+          }}
+        >
+          {rating >= 1 ? <AiFillStar /> : <AiOutlineStar />}
+        </button>
+        <button
+          onClick={() => {
+            setRating(2);
+          }}
+        >
+          {rating >= 2 ? <AiFillStar /> : <AiOutlineStar />}
+        </button>
+        <button
+          onClick={() => {
+            setRating(3);
+          }}
+        >
+          {rating >= 3 ? <AiFillStar /> : <AiOutlineStar />}
+        </button>
+        <button
+          onClick={() => {
+            setRating(4);
+          }}
+        >
+          {rating >= 4 ? <AiFillStar /> : <AiOutlineStar />}
+        </button>
+        <button
+          onClick={() => {
+            setRating(5);
+          }}
+        >
+          {rating >= 5 ? <AiFillStar /> : <AiOutlineStar />}
+        </button>
+        {rating} / 5
+      </div>
+      <div className="w-full]">
+        <textarea className="w-full border" />
       </div>
       <div>
-        <input type="file" name="" id="" />
+        <input type="file" name="" id="" onChange={handleImageChange} />
       </div>
       <div>
-        <button className="border w-[calc(100%-2rem)] rounded-md h-10">제출</button>
+        <button className="border w-full rounded-md h-10">제출</button>
       </div>
-    </div>
+    </form>
   );
 };

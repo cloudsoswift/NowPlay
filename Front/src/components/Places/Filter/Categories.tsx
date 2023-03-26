b30e0e76c8b6f4eace0d07594905fe9d8697ef23
박규영 <magic9282m@gmail.com>
2023-03-27 04:00:27 +0900

    feat: 선택된 카테고리 알약의 close 버튼 클릭시 카테고리 선택 취소하도록 수정

--- Front/src/components/Places/Filter/Categories.tsx
+++ Front/src/components/Places/Filter/Categories.tsx
@@ -1,29 +1,33 @@
-// @flow 
-import * as React from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { filterState } from '../Map';
-import { TSubCategory } from '../Types';
-type CategoriesProps = {
-
-};
+// @flow
+import * as React from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { filterState } from "../Map";
+import { TSubCategory } from "../Types";
+import { RxCross2 } from "react-icons/rx";
+type CategoriesProps = {};
 type CategoryProps = {
-  subCategory: TSubCategory
+  subCategory: TSubCategory;
 };
 
-export const Category = ({subCategory}: CategoryProps) => {
+export const Category = ({ subCategory }: CategoryProps) => {
   const setFilter = useSetRecoilState(filterState);
   const handleCategoryClick = () => {
-    setFilter((prevFilter)=>{
+    setFilter((prevFilter) => {
       return {
         ...prevFilter,
-        selectedCategories: prevFilter.selectedCategories.map((prevCategories)=>prevCategories.category !== subCategory.category),
-      }
-    })
-  }
+        selectedCategories: prevFilter.selectedCategories.filter(
+          (prevCategories) => prevCategories.category !== subCategory.category
+        ),
+      };
+    });
+  };
   return (
-    <span className="border-2 rounded-full p-2">
-      {subCategory.category}
-    </span>
+    <div className="border-2 rounded-full p-2 inline">
+      <span className="" >
+        {subCategory.category}
+      </span>
+      <button type="button" onClick={handleCategoryClick} className="align-middle"><RxCross2 /></button>
+    </div>
   );
 };
 
@@ -31,7 +35,9 @@ export const Categories = (props: CategoriesProps) => {
   const { selectedCategories } = useRecoilValue(filterState);
   return (
     <div className="w-full overflow-x-scroll space-x-2 whitespace-nowrap p-2 my-2">
-      {selectedCategories.map((data)=><Category key={data.category} subCategory={data}/>)}
+      {selectedCategories.map((data) => (
+        <Category key={data.category} subCategory={data} />
+      ))}
     </div>
   );
 };

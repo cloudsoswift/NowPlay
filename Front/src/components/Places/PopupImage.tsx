3e93e18ed7ff973084b9a8c3cc7f9c3a3408ba62
박규영 <magic9282m@gmail.com>
2023-03-27 03:59:24 +0900

    feat: 별점 표시를 위한 컴포넌트 및 이미지 클릭시 크게 보여주는 컴포넌트 추가

---
+++ Front/src/components/Places/PopupImage.tsx
@@ -0,0 +1,27 @@
+import { Dialog } from "@headlessui/react";
+import { useState } from "react";
+type Props = {
+  imageURL: string;
+  imageClass: string;
+};
+export const PopupImage = ({ imageURL, imageClass }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className={imageClass}>
+      <img src={imageURL} alt="" className="" onClick={() => setIsOpen(true)} />
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="fixed inset-0 bg-black bg-opacity-25 z-10"
+      >
+        <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl">
+            <Dialog.Title>사진 자세히 보기</Dialog.Title>
+            <img src={`${imageURL}`} className="w-full h-full p-8" />
+            <button onClick={() => setIsOpen(false)}>닫기</button>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+    </div>
+  );
+};

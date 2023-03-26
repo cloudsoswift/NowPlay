9a7b67a12aa948846493c7ea2b0019ad5d5d3948
dreamjh1111 <dreamjh1111@gmail.com>
2023-03-27 03:06:17 +0900

    Feature: 예약하기 인터페이스 생성

---
+++ Back/specialized/src/main/java/com/ssafy/specialized/service/ReservationService.java
@@ -0,0 +1,15 @@
+package com.ssafy.specialized.service;
+
+public interface ReservationService {
+    // 예약 요청을 처리하는 메서드
+    ReservationDto reserve(ReservationRequestDto reservationRequestDto);
+
+    // 예약 정보를 조회하는 메서드
+    ReservationDto getReservation(int reservationIdx);
+
+    // 예약 정보를 수정하는 메서드
+    ReservationDto updateReservation(int reservationIdx, ReservationRequestDto reservationRequestDto);
+
+    // 예약 정보를 삭제하는 메서드
+    void deleteReservation(int reservationIdx);
+}

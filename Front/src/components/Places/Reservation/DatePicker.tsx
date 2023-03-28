4a90f5a3a0e80c5ee3e29d7647760d7bcab223d3
cloudsoswift <magic9282m@gmail.com>
2023-03-28 18:03:00 +0900

    feat: DatePicker 컴포넌트 일부 수정

--- Front/src/components/Places/Reservation/DatePicker.tsx
+++ Front/src/components/Places/Reservation/DatePicker.tsx
@@ -1,24 +1,41 @@
 import moment from "moment";
+
 import { useState } from "react";
 type Props = {
   onClick: Function;
 };
 
 const Calendar = () => {
-  const date = moment();
-  console.log(date);
+  const [calendarDate, setCalendarDate] = useState(moment());
+  const [selectedDate, setSelectedDate] = useState(moment());
+  const arr = Array<JSX.IntrinsicElements>();
+  const allWeek = calendarDate.daysInMonth()/7
+  console.log(calendarDate.week());  
+  console.log(calendarDate.day(1).add(1, 'week').format());
+  arr.push()
+  // for(let week = 1; week < calendarDate.daysInMonth()/7; week++){
+  //   arr.push(
+  //     <td>
+  //       {for(let day = week)}
+  //     </td>
+  //   )
+  // }
   console.log(moment().day(1).format("ddd"));
+  console.log(calendarDate.format());
+  console.log(calendarDate.startOf("month").format());
+  console.log(calendarDate.startOf("month").day());
+  
   return <div>
-    <table>
+    <table className="w-full h-full text-center">
       <thead>
         <tr>
-          <td>일</td>
           <td>월</td>
           <td>화</td>
           <td>수</td>
           <td>목</td>
           <td>금</td>
           <td>토</td>
+          <td>일</td>
         </tr>
       </thead>
       <tbody>
@@ -31,6 +48,9 @@ const Calendar = () => {
           <td>ㅇ</td>
           <td>ㅇ</td>
         </tr>
+        {/* {
+          Array.from()
+        } */}
       </tbody>
     </table>
   </div>

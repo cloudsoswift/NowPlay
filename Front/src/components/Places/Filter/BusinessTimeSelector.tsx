cff68e7adf70ce0cda862c45ebbf243e9f387af5
박규영 <magic9282m@gmail.com>
2023-03-30 02:52:22 +0900

    feat: 필터 - 영업시간 선택시 '상관 없음' 옵션 추가

--- Front/src/components/Places/Filter/BusinessTimeSelector.tsx
+++ Front/src/components/Places/Filter/BusinessTimeSelector.tsx
@@ -5,6 +5,7 @@ import { filterState } from "../Map";
 type Props = {};
 export const BusinessTimeSelector = (props: Props) => {
   const setFilter = useSetRecoilState(filterState);
+  const [isEnabled, setIsEnabled] = useState(true);
   const nowHour = new Date().getHours();
   const nowMinute = new Date().getMinutes();
   const [hour, setHour] = useState(nowHour);
@@ -17,26 +18,39 @@ export const BusinessTimeSelector = (props: Props) => {
     });
   };
   return (
-    <div className="flex justify-center w-full border rounded-xl p-2">
-      <select name="" id="" onChange={(e)=>{setHour(Number(e.target.value))}}>
-        {Array.from(Array(25 - nowHour).keys()).map((v) => (
-          <option value={v + nowHour}>{v + nowHour}</option>
-        ))}
-      </select>
-      시
-      <select name="" id="">
-        {
-          <option value={0} disabled={nowMinute > 0 && hour <= nowHour}>
-            0
-          </option>
-        }
-        {
-          <option value={0} disabled={nowMinute > 30 && hour <= nowHour}>
-            30
-          </option>
-        }
-      </select>
-      분 이후
+    <div className="flex justify-around w-full border rounded-xl p-2">
+      <span className="">
+        <select
+          name=""
+          id=""
+          onChange={(e) => {
+            setHour(Number(e.target.value));
+          }}
+          disabled={!isEnabled}
+        >
+          {Array.from(Array(25 - nowHour).keys()).map((v) => (
+            <option value={v + nowHour}>{v + nowHour}</option>
+          ))}
+        </select>
+        시
+        <select name="" id="" disabled={!isEnabled}>
+          {
+            <option value={0} disabled={nowMinute > 0 && hour <= nowHour}>
+              0
+            </option>
+          }
+          {
+            <option value={30} disabled={nowMinute > 30 && hour <= nowHour}>
+              30
+            </option>
+          }
+        </select>
+        분 이후
+      </span>
+      <span>
+        <input type="checkbox" id="isEnabled" onChange={()=>{setIsEnabled((prevState)=>!prevState)}}/>
+        <label htmlFor="isEnabled">상관 없음</label>
+      </span>
     </div>
   );
 };

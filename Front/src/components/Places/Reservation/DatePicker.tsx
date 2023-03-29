dd7a6ce398ef4acc600e7709aaa831392e47d428
cloudsoswift <magic9282m@gmail.com>
2023-03-29 17:59:37 +0900

    Feat: 예약 일자 고르는 DatePicker 컴포넌트 완성

--- Front/src/components/Places/Reservation/DatePicker.tsx
+++ Front/src/components/Places/Reservation/DatePicker.tsx
@@ -1,66 +1,157 @@
-import moment from "moment";
+import moment, { Moment } from "moment";
 
 import { useState } from "react";
-type Props = {
-  onClick: Function;
+import { SetterOrUpdater, useRecoilState } from "recoil";
+import { reservationInfo, TReservationInfo } from "../../../pages/places/PlaceReservationPage";
+type DayProps = {
+  onClick: SetterOrUpdater<TReservationInfo>;
+  isSelected: boolean;
+  isPresentMonth: boolean;
+  date: Moment;
+};
+const Day = ({ onClick, isSelected, isPresentMonth, date }: DayProps) => {
+  const weekday = date.weekday();
+  return (
+    <td
+      className={`${(!isPresentMonth && "text-opacity-30") || ""} ${
+        (isSelected && "text-white bg-[var(--primary-color)]") || 
+        (weekday === 6 && "text-blue-500") ||
+        (weekday === 0 && "text-red-500") || "text-black"
+      } py-2`}
+      onClick={() => {
+        onClick((prevReservationInfo)=>{
+          return {
+            ...prevReservationInfo,
+            dateTime: date,
+            dateChecked: true,
+          }
+        });
+      }}
+    >
+      {date.date()}
+    </td>
+  );
 };
 
 const Calendar = () => {
   const [calendarDate, setCalendarDate] = useState(moment());
-  const [selectedDate, setSelectedDate] = useState(moment());
-  const arr = Array<JSX.IntrinsicElements>();
-  const allWeek = calendarDate.daysInMonth()/7
-  console.log(calendarDate.week());  
-  console.log(calendarDate.day(1).add(1, 'week').format());
-  arr.push()
-  // for(let week = 1; week < calendarDate.daysInMonth()/7; week++){
-  //   arr.push(
-  //     <td>
-  //       {for(let day = week)}
-  //     </td>
-  //   )
-  // }
-  console.log(moment().day(1).format("ddd"));
-  console.log(calendarDate.format());
-  console.log(calendarDate.startOf("month").format());
-  console.log(calendarDate.startOf("month").day());
-  
-  return <div>
-    <table className="w-full h-full text-center">
-      <thead>
-        <tr>
-          <td>월</td>
-          <td>화</td>
-          <td>수</td>
-          <td>목</td>
-          <td>금</td>
-          <td>토</td>
-          <td>일</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>ㅇ</td>
-          <td>ㅇ</td>
-          <td>ㅇ</td>
-          <td>ㅇ</td>
-          <td>ㅇ</td>
-          <td>ㅇ</td>
-          <td>ㅇ</td>
-        </tr>
-        {/* {
-          Array.from()
-        } */}
-      </tbody>
-    </table>
-  </div>
-}
+  const [{dateTime}, setReservationInfo] = useRecoilState(reservationInfo);
+
+  const dayOfMonth = calendarDate.daysInMonth();
+  const previousMonth = moment(calendarDate).subtract(1, "month");
+  const dayOfPreviousMonth = previousMonth.daysInMonth();
+  const nextMonth = moment(calendarDate).add(1, "month");
+  let previousMonthLWS =
+    moment(previousMonth).date(dayOfPreviousMonth).weekday() !== 6
+      ? moment(previousMonth).date(dayOfPreviousMonth).startOf("week").date()
+      : dayOfPreviousMonth + 1;
+  let presentMonthCount = 1;
+  let nextMonthFWSCount = 1;
+  let nextMonthFWS = moment(nextMonth).date(1).endOf("week").date() + 7;
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <button
+          onClick={() => {
+            setCalendarDate((prevDate) =>
+              moment(prevDate).subtract(1, "month")
+            );
+          }}
+        >
+          이전 달로
+        </button>
+        <span>
+          <span className="text-xl mr-1">{calendarDate.year()}</span>
+          <span>{calendarDate.month() + 1}</span>
+        </span>
+        <button
+          onClick={() => {
+            setCalendarDate((prevDate) => moment(prevDate).add(1, "month"));
+          }}
+        >
+          다음 달로
+        </button>
+      </div>
+      <table className="w-full h-full text-center">
+        <thead>
+          <tr>
+            <td>일</td>
+            <td>월</td>
+            <td>화</td>
+            <td>수</td>
+            <td>목</td>
+            <td>금</td>
+            <td>토</td>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(Array(6).keys()).map((week) => {
+            return (
+              <tr>
+                {Array.from(Array(7).keys()).map((i) => {
+                  // 현재 달의 첫 날이 일요일이 아닌 경우
+                  // 달력에서 지난 달 마지막 주 ~ 현재 달의 첫 날 표시하는 부분
+                  if (previousMonthLWS <= dayOfPreviousMonth) {
+                    // return <td>{previousMonthLWS++}</td>
+                    return (
+                      <Day
+                        date={moment(previousMonth).date(previousMonthLWS++)}
+                        isSelected={moment(previousMonth)
+                          .date(previousMonthLWS - 1)
+                          .isSame(dateTime, 'day')}
+                        onClick={setReservationInfo}
+                        isPresentMonth={false}
+                      />
+                    );
+                  } else if (presentMonthCount <= dayOfMonth) {
+                    return (
+                      <Day
+                        date={moment(calendarDate).date(presentMonthCount++)}
+                        isSelected={moment(calendarDate)
+                          .date(presentMonthCount - 1)
+                          .isSame(dateTime, 'day')}
+                        onClick={setReservationInfo}
+                        isPresentMonth={true}
+                      />
+                    );
+                    // 현재 달의 마지막 날 ~ 다음 달의 두 번째 주(현재 달의 마지막 날이 토요일이면 첫 번째 주 까지만) 표시하는 부분
+                  } else if (nextMonthFWSCount <= nextMonthFWS) {
+                    return (
+                      <Day
+                        date={moment(nextMonth).date(nextMonthFWSCount++)}
+                        isSelected={moment(nextMonth)
+                          .date(nextMonthFWSCount - 1)
+                          .isSame(dateTime, 'day')}
+                        onClick={setReservationInfo}
+                        isPresentMonth={false}
+                      />
+                    );
+                  }
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export const DatePicker = (props: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className={`w-[calc(100vw-32px)] ${isOpen ? "h-[calc(100vw-32px)]" : ""} border`}>
-      <div className="w-full border text-2xl p-2" onClick={()=>setIsOpen((prevState)=>!prevState)}>1. 날짜 선택</div>
+    <div
+      className={`w-[calc(100vw-32px)] ${
+        isOpen ? "h-[calc(100vw-32px)]" : ""
+      } border`}
+    >
+      <div
+        className="w-full border text-2xl p-2"
+        onClick={() => setIsOpen((prevState) => !prevState)}
+      >
+        1. 날짜 선택
+      </div>
       {isOpen && <Calendar />}
     </div>
   );

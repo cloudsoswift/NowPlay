fe25daa56917ec687baa4deb21aed29fcb1d9142
이흥종 <avjg123@naver.com>
2023-04-01 03:23:41 +0900

    Fix: CORS 고치기 위해 GraphQlController에 RequestMapping 붙여봄

--- Back/specialized/src/main/java/com/ssafy/specialized/controller/GraphQlController.java
+++ Back/specialized/src/main/java/com/ssafy/specialized/controller/GraphQlController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @CrossOrigin(origins = {"https://j8d110.p.ssafy.io", "http://127.0.0.1:5173", "http://localhost:5173", "http://172.30.1.95"}, allowCredentials = "true")
+@RequestMapping("/graphql")
 public class GraphQlController {
 
     @Autowired

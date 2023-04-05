e2b23ecc1a9c6d5369bfb9f2a5c4c7259f614cb6
cloudsoswift <magic9282m@gmail.com>
2023-04-05 17:48:43 +0900

    feat: 예약 페이지 - 가게 영업시간에 맞게 날짜 및 시간 선택 하도록 연결.

--- Front/src/components/Places/Reservation/TimePicker.tsx
+++ Front/src/components/Places/Reservation/TimePicker.tsx
@@ -1,12 +1,16 @@
-import moment, { Moment } from 'moment';
+import moment, { duration, Moment } from 'moment';
 import { useState } from 'react';
 import { SetterOrUpdater, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { reservationInfo, TReservationInfo } from '../../../pages/places/PlaceReservationPage';
+import { TBusinessHour } from '../../../utils/api/graphql';
+import { ENUM_DAYS, WEEK_OF_DAYS } from './DatePicker';
 type Props = {
+  businessHour: TBusinessHour[] 
 };
 type TimeProps = {
   time: Moment;
   isSelected: boolean;
+  // businessHour: TBusinessHour[] 
 }
 const Time = ({time, isSelected}: TimeProps) => {
   const onClick = useSetRecoilState(reservationInfo);
@@ -21,28 +25,31 @@ const Time = ({time, isSelected}: TimeProps) => {
   }
   return <div className={`text-center py-2 border ${isSelected ? "text-white bg-[var(--primary-color)]" : ""}`} onClick={handleClick}>{`${time.format('HH:mm')}`}</div>
 }
-const TimeList = ({date}: {date: Moment}) => {
-  const today = moment(date).hour(9).minute(0);
+const TimeList = ({date, businessHour}: {date: Moment, businessHour: TBusinessHour[]}) => {
+  const today = moment(date).hour(0).minute(0);
+  const todayBusinessHour = businessHour.find(bh=>{
+    const key: ENUM_DAYS = bh.dayOfWeek as ENUM_DAYS;
+    return WEEK_OF_DAYS[key] === today.weekday();
+  })
   const arr = Array();
-  console.log(today.isAfter(moment(date).hour(18).minute(0)), today.format(), moment(date).hour(18).minute(0).format());
-  while(!today.isAfter(moment(date).hour(18).minute(0))){
-    console.log(today.format());
+  const closeHour = moment(today).add(moment.duration(todayBusinessHour?.close))
+  today.add(moment.duration(todayBusinessHour?.open));
+  while(!today.isAfter(closeHour)){
     arr.push(<Time time={moment(today)} isSelected={date.isSame(moment(today))}/>)
-    today.add(30, "minute")
+    today.add(todayBusinessHour?.reservationInterval || 30, "minute")
   }
-  console.log(today.format());
   return <div className='grid grid-cols-3'>
     {arr.map((d)=>d)}
   </div>
 }
 
-export const TimePicker = (props: Props) => {
+export const TimePicker = ({ businessHour }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const {dateTime} = useRecoilValue(reservationInfo);
+  const {dateTime, dateChecked} = useRecoilValue(reservationInfo);
   return (
     <div className={`w-[calc(100vw-32px)] ${isOpen ? "h-[calc(100vw-32px)]" : ""} border`}>
-      <div className="w-full border text-2xl p-2" onClick={()=>setIsOpen((prevState)=>!prevState)}>2. 시간 선택</div>
-      {isOpen && <TimeList date={dateTime}/>}
+      <div className="w-full border text-2xl p-2" onClick={()=>{dateChecked ? setIsOpen((prevState)=>!prevState) : alert('날짜를 먼저 선택해주세요.')}}>2. 시간 선택</div>
+      {isOpen && <TimeList date={dateTime} businessHour={businessHour} />}
     </div>
   );
 };
--- Front/src/pages/places/PlaceReservationPage.tsx
+++ Front/src/pages/places/PlaceReservationPage.tsx
@@ -1,8 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
 import moment from "moment";
+import { useNavigate, useParams } from "react-router-dom";
 import { atom, useRecoilState, useRecoilValue } from "recoil";
 import { DatePicker } from "../../components/Places/Reservation/DatePicker";
 import { RequestForm } from "../../components/Places/Reservation/RequestForm";
 import { TimePicker } from "../../components/Places/Reservation/TimePicker";
+import api from "../../utils/api/api";
+import { TStoreDetail } from "../../utils/api/graphql";
+import { userInfoAtom } from "../../utils/recoil/userAtom";
 
 type Props = {
   
@@ -16,12 +21,39 @@ export type TReservationInfo = {
 export const reservationInfo = atom<TReservationInfo>({key: "reservationInfo", default: {dateTime: moment(), request: "", dateChecked: false, timeChecked: false,}});
 export const PlaceReservationPage = (props: Props) => {
   const reservationInfoValue = useRecoilValue(reservationInfo);
+  // const {userIdx} = useRecoilValue(userInfoAtom);
+  const {id} = useParams();
+  const fetchStoreDetail = () => {
+    return api.get(`place/${id}/store`).then(({data})=>data);
+  }
+  const {data, error, isLoading} = useQuery<TStoreDetail>(["storeDetail"], fetchStoreDetail);
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    console.log(reservationInfoValue.dateTime.format());
+    api.post("reservation", {
+      "history": reservationInfoValue.request,
+      "reserverIdx": 1,
+      "storeIdx": id,
+      "time": reservationInfoValue.dateTime.format('YYYY-MM-DDTHH:mm:ss[Z]')
+    }).then((response)=>{
+      switch(response.status){
+        case 200:
+        case 201:
+          alert("예약에 성공했습니다.");
+          navigate(`/mobile/places/${id}`);
+          break;
+        default:
+          alert("예약에 실패했습니다. 다시 시도하거나 다른 날짜를 선택해주세요.");
+          break;
+      }
+    })
+  }
   return (
     <div className="px-4 h-[calc(100vh-128px)] overflow-y-scroll">
-      <DatePicker />
-      <TimePicker />
+      <DatePicker businessHour={data?.businessHourList || []} />
+      <TimePicker businessHour={data?.businessHourList || []} />
       <RequestForm />
-      <button className="w-full border" disabled={!(reservationInfoValue.dateChecked && reservationInfoValue.timeChecked && reservationInfoValue.request.trim().length > 0)}>예약하기</button>
+      <button className="w-full border" disabled={!(reservationInfoValue.dateChecked && reservationInfoValue.timeChecked && reservationInfoValue.request.trim().length > 0)} onClick={handleSubmit}>예약하기</button>
     </div>
   );
 };
